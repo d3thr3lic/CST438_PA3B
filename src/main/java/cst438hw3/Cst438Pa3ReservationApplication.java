1e088eeb83ca9ad2fdc067612463ba65c82765ca
Mitchell Saunders <d3thr3lic@gmail.com>
2020-07-09 16:38:16 -0700

    Completed. Ready for review.

---
+++ src/main/java/cst438hw3/Cst438Pa3ReservationApplication.java
@@ -0,0 +1,13 @@
+package cst438hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Pa3ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Pa3ReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cst438hw3/Cst438Pa3ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Pa3ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
