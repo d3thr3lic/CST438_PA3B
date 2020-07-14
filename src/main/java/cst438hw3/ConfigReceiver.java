004ff5366d08b9939cd6325fddb28f0ff4d29639
Chaoslime <nicholas3000@sbcglobal.net>
2020-07-13 19:47:40 -0700

    Code looks good, some formatting agian though.

--- src/main/java/cst438hw3/ConfigReceiver.java
+++ src/main/java/cst438hw3/ConfigReceiver.java
@@ -9,20 +9,23 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigReceiver {
-    @Bean
-    public FanoutExchange fanout() {
-        return new FanoutExchange("${queueName}");
-    }
-    @Bean
-    public Queue queue1() {
-        return new Queue("${queueName}");
-    }
-    @Bean
-    public Binding binding1(FanoutExchange fanout, Queue queue1) {
-        return BindingBuilder.bind(queue1).to(fanout);
-    }
-    @Bean
-    public ReservationEventHandler receiver() {
-        return new ReservationEventHandler();
-    }
-} 
+  @Bean
+  public FanoutExchange fanout() {
+    return new FanoutExchange("${queueName}");
+  }
+
+  @Bean
+  public Queue queue1() {
+    return new Queue("${queueName}");
+  }
+
+  @Bean
+  public Binding binding1(FanoutExchange fanout, Queue queue1) {
+    return BindingBuilder.bind(queue1).to(fanout);
+  }
+
+  @Bean
+  public ReservationEventHandler receiver() {
+    return new ReservationEventHandler();
+  }
+}
--- src/main/java/cst438hw3/Cst438Pa3ReservationApplication.java
+++ src/main/java/cst438hw3/Cst438Pa3ReservationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cst438Pa3ReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cst438Pa3ReservationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Cst438Pa3ReservationApplication.class, args);
+  }
 
 }
